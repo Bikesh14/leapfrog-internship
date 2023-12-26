5ed6fd54aef470bf784d2cea1a4e28e85a1b5ca0
Bikesh Bimali <bimalibikesh@gmail.com>
2023-12-26 11:08:39 +0545

    add: Todo list in typescript with Docker

---
+++ todo_TS/vite.config.js
@@ -0,0 +1,12 @@
+// vite.config.js
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  server: {
+    hmr: {
+      host: "0.0.0.0",
+      port: 5173,
+    },
+  },
+  base: "./",
+});
