473d6cbbd333b951211358aa4d717e4823ff711d
Bikesh Bimali <bimalibikesh@gmail.com>
2024-01-15 22:19:43 +0545

    add: final project link

--- database_task_1/migrations/Customers.sql
+++ database_task_1/migrations/Customers.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Customers (
     CustomerID SERIAL PRIMARY KEY,
     CustomerName VARCHAR(255),
-    CustomerName VARCHAR(255),
+    ContactName VARCHAR(255),
     Address VARCHAR(255),
     City VARCHAR(255),
     PostalCode VARCHAR(255),
